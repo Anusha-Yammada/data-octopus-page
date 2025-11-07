67440edc356d933350476f3096a9dc8f56f4069f
Anusha-Yammada <anuyammada@gmail.com>
2025-11-06 20:07:51 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/dataoctopus-page/",
+  base: "/data-octopus-page/", 
   server: {
     port: 5000,
     host: '0.0.0.0',
