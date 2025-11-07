57ff30f5280dfe23298301f522e42cd102e7a767
Anusha-Yammada <anuyammada@gmail.com>
2025-11-07 01:28:00 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/data-octopus-page/", 
-  server: {
-    port: 5000,
-    host: '0.0.0.0',
-    strictPort: true,
-  },
-});
+  base: '/data-octopus-page/', // must match your GitHub repo name
+})
